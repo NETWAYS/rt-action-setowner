4eda86ba8dfa971a78900ca0be4c32a688b8dcaa
Johannes Meyer <johannes.meyer@netways.de>
2018-03-16 08:07:08 +0100

    Change naming schema from RTx::Action to RT::Action

--- lib/RT/Action/SetOwner.pm
+++ lib/RT/Action/SetOwner.pm
@@ -9,23 +9,25 @@ use subs qw(
     UserArgument
 );
 
+our $VERSION="1.0.0";
+
 sub Prepare {
     my $self = shift;
     RT->Logger->debug(__PACKAGE__. ': PREPARE');
-    
+
     my $userid = $self->UserArgument();
-    
+
     RT->Logger->debug(__PACKAGE__. ': Try to load user '. $userid);
-    
+
     my $user = undef;
-    
+
     if ($userid eq 'RT::Nobody') {
     	$user = RT->Nobody;
     } else {
         $user = RT::User->new(RT->SystemUser);
         $user->Load($userid);
     }
-    
+
     if ($user && $user->Id > 0) {
         RT->Logger->error(__PACKAGE__. ': Got user: '. $user->Name);
         $self->{user} = $user;
@@ -38,7 +40,7 @@ sub Prepare {
 
 sub Commit {
     my $self = shift;
-    
+
     if (ref($self->{user}) && $self->{user}->Id()) {
         $self->TicketObj->setOwner($self->{user}->Id(), 'Force');
     }
@@ -46,13 +48,13 @@ sub Commit {
 
 sub UserArgument {
     my $self = shift;
-    
+
     my $userid = $self->TemplateObj->Content;
-    
+
     unless ($userid) {
     	$userid = $self->Argument;
     }
-    
+
     return $userid;
 }
 
